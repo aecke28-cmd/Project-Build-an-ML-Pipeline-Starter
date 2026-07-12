2c51a2c9da568f915095614f8a8fad15d01d52eb
aecke28-cmd <aecke28@wgu.edu>
2026-07-12 17:45:23 -0400

    added test model to pipeline

--- main.py
+++ main.py
@@ -133,7 +133,15 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                env_manager="conda",
+                parameters = {
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
             pass
 
 
